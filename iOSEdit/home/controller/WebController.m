a3a23144d5c89dde6f2f65a62461fc3abde119ac
Stan <573387383@QQ.COM>
2021-02-22 18:35:21 +0800

    v2

--- iOSEdit/home/controller/WebController.m
+++ iOSEdit/home/controller/WebController.m
@@ -9,11 +9,14 @@
 #import <WebKit/WebKit.h>
 #import "ScriptDelegate.h"
 
+
+
 @interface WebController ()
 <WKNavigationDelegate,
 WKScriptMessageHandler,
 WKUIDelegate>
 @property(nonatomic)WKWebView *wkWebView;
+@property(nonatomic)UIView *bottomView;
 @end
 
 @implementation WebController
@@ -54,7 +57,43 @@ WKUIDelegate>
     
     return config;
 }
-
+-(UIView *)bottomView
+{
+    if (!_bottomView) {
+        _bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height-44, [UIScreen mainScreen].bounds.size.width, 44)];
+        _bottomView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.2];
+        
+        UIButton *boldButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [boldButton setImage:[UIImage imageNamed:@"B"] forState:UIControlStateNormal];
+        [boldButton setImage:[UIImage imageNamed:@"BHOVER"] forState:UIControlStateSelected];
+        boldButton.frame = CGRectMake(0, 0, 44, 44);
+        [_bottomView addSubview:boldButton];
+        [boldButton addTarget:self action:@selector(boldButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        UIButton *italicsButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [italicsButton setImage:[UIImage imageNamed:@"I"] forState:UIControlStateNormal];
+        [italicsButton setImage:[UIImage imageNamed:@"IHOVER"] forState:UIControlStateSelected];
+        italicsButton.frame = CGRectMake(44, 0, 44, 44);
+        [_bottomView addSubview:italicsButton];
+        [italicsButton addTarget:self action:@selector(italicsButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        UIButton *underlineButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [underlineButton setImage:[UIImage imageNamed:@"u"] forState:UIControlStateNormal];
+        [underlineButton setImage:[UIImage imageNamed:@"uHOVER"] forState:UIControlStateSelected];
+        underlineButton.frame = CGRectMake(44*2, 0, 44, 44);
+        [_bottomView addSubview:underlineButton];
+        [underlineButton addTarget:self action:@selector(underlineButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+//        UIButton *boldButton = [UIButton buttonWithType:UIButtonTypeCustom];
+//        [boldButton setImage:[UIImage imageNamed:@"B"] forState:UIControlStateNormal];
+//        [boldButton setImage:[UIImage imageNamed:@"BHOVER"] forState:UIControlStateSelected];
+//        boldButton.frame = CGRectMake(44*3, 0, 44, 44);
+//        _bottomView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.2];
+//        [_bottomView addSubview:boldButton];
+        
+    }
+    return _bottomView;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     //加载本地的html
@@ -62,11 +101,84 @@ WKUIDelegate>
     NSString *htmlString = [[NSString alloc]initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
     [self.wkWebView loadHTMLString:htmlString baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]]];
     [self.view addSubview:self.wkWebView];
+    [self.view addSubview:self.bottomView];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    
 }
 #pragma mark-WKScriptMessageHandler
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
 {
     
 }
+
+/**
+ *   键盘弹出
+ */
+- (void)keyboardWillShow:(NSNotification *)note
+{
+    
+    // 1.取出键盘的高度
+    CGRect temp  = [note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGFloat height = temp.size.height;
+    // 2.让工具条向上平移
+    // 2.1取出键盘弹出的动画时间
+    NSTimeInterval timte = [note.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    [UIView animateWithDuration:timte delay:0 options:7 << 16 animations:^{
+        self.bottomView.transform = CGAffineTransformMakeTranslation(0, -height);
+    } completion:nil];
+    
+}
+/**
+ *  键盘隐藏
+ */
+- (void)keyboardWillHide:(NSNotification *)note
+{
+    // 2.1取出键盘弹出的动画时间
+    NSTimeInterval timte = [note.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    // 清空transform
+    [UIView animateWithDuration:timte delay:0 options:7 << 16 animations:^{
+        self.bottomView.transform = CGAffineTransformIdentity;
+    } completion:nil];
+    
+}
+
+-(void)boldButtonClick:(UIButton*)send
+{
+    send.selected = !send.selected;
+    if (send.isSelected) {
+        [self ocTojs:@"bold"];
+    }else{
+        [self ocTojs:@"unbold"];
+    }
+}
+
+-(void)italicsButtonClick:(UIButton*)send
+{
+    send.selected = !send.selected;
+    if (send.isSelected) {
+        [self ocTojs:@"italics"];
+    }
+}
+
+-(void)underlineButtonClick:(UIButton*)send
+{
+    send.selected = !send.selected;
+    if (send.isSelected) {
+        [self ocTojs:@"underline"];
+    }
+}
+
+
+-(void)ocTojs:(NSString*)action
+{
+    NSString *jsString = [NSString stringWithFormat:@"ocTojsAction('%@')",action];
+    [self.wkWebView evaluateJavaScript:jsString completionHandler:^(id _Nullable data, NSError * _Nullable error) {
+    }];
+}
+
 
 @end
